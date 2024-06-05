bd35425d600b48788b9c02f59f2f16f6192a7e4a
omar <onarmohanad@gmail.com>
2024-06-05 23:52:41 +0300

    tassk function

---
+++ app.js
@@ -0,0 +1,45 @@
+/*q1 */
+// let num = [5,3,2,4,4,3];
+// function reversenum(){
+//     let numev =[];
+//     for(let i = num.length-1; i>=0; i--){
+//     numev +=num[i];
+//     }
+//     return numev;
+// }
+// alert(reversenum());
+
+/*q2 */
+    
+// for (let i = 0 ; i<=15; i++){
+//     if(i%2 == 0){
+//         console.log(`${i} is even`);
+//     }
+//    else{
+//     console.log(`${i} is odd`);
+//    }
+// }
+
+
+/*q3 */
+
+// function string(){
+//   let x = "orange";
+//   return console.log(x.split("").sort().join(""))
+// }
+// string();
+
+
+/*4 */
+
+// git add
+
+
+
+/*q5 */
+
+// function age(z){
+//     let x = z >=18 ? 'The user is Adult': 'The user is Minor';
+//     return console.log(x);
+// }
+// age(20);
